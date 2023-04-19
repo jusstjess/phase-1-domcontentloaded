f804d2c09e9abfb90d32277375ae74f089e340f9
Jess Leyva <jessica.leyvat@yahoo.com>
2023-04-19 10:21:24 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.querySelector('#text')
+  element.textContent = "This is really cool!"
+  });
+
+  
+  
